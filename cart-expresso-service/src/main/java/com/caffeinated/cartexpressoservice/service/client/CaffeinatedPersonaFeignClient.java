d23daf4e912daabf0c9610fd35c4f45f024375a3
Swati-Shambhavi <swatishambhavi.09@gmail.com>
2024-01-08 16:47:51 +0530

    Dockerized the 3 microservices using Google jib

--- cart-expresso-service/src/main/java/com/caffeinated/cartexpressoservice/service/client/CaffeinatedPersonaFeignClient.java
+++ cart-expresso-service/src/main/java/com/caffeinated/cartexpressoservice/service/client/CaffeinatedPersonaFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "caffeinated-persona-service", url = "localhost:8081/api/users")
+@FeignClient(name = "caffeinated-persona-service", url = "192.168.1.36:8081/api/users")
 public interface CaffeinatedPersonaFeignClient {
     @GetMapping("/{email}")
     public ServiceResponse getUserDetail(@PathVariable String email);
--- cart-expresso-service/src/main/java/com/caffeinated/cartexpressoservice/service/client/ProductCraftsmanFeignClient.java
+++ cart-expresso-service/src/main/java/com/caffeinated/cartexpressoservice/service/client/ProductCraftsmanFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-craftsman-service", url = "localhost:8082/api/products")
+@FeignClient(name = "product-craftsman-service", url = "192.168.1.36:8082/api/products")
 public interface ProductCraftsmanFeignClient {
     @GetMapping("/{productId}")
     public ServiceResponse getProduct(@PathVariable Integer productId) throws Exception;
